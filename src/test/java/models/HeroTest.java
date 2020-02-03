d078234a0cc600ba9ba1f2cdb26e43cda33936ac
Andrew-1000 <andruambia95@gmail.com>
2020-02-03 12:24:03 +0300

    Add HeroTest class to the models package in test directory

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,4 +1,36 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class HeroTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void NewHeroObjectGetsCreatedCorrectly_true() {
+        Hero hero = setupNewHero();
+        assertEquals(true, hero instanceof Hero);
+    }
+
+    @Test
+    public void HeroInstatiatesWithName_true() {
+        Hero hero = setupNewHero();
+        assertEquals("Spiderman", hero.getName());
+    }
+
+    private Hero setupNewHero() {
+        return new Hero("Spiderman", 1, 23, "Genius-level intellect", "death");
+    }
 }
